a72a779bec99c9475994b672a6885d877febb396
Leonardo Henrique <90967601+DevLeo13@users.noreply.github.com>
2023-04-03 11:50:05 -0300

    Add files via upload

---
+++ aula 9.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é ${vel}Km/h `)
+if (vel > 60){
+    console.log('Você ultrapassou o limite permitido! MULTADO')
+}
+else{
+    console.log('Parabens você está dirigindo com segurança!')
+}
